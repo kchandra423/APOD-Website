404e852e84b2d9812826d72cbc961d9a83521891
Kumar.s.Chandra <kchandra423@student.fuhsd.org>
2023-01-11 21:39:29 -0800

    created requirements

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import urllib
-
 import requests
 import streamlit as st
-import pandas as pd
-import numpy as np
 from bs4 import BeautifulSoup
 
 raw_html = requests.get('https://apod.nasa.gov/apod/astropix.html').text
-tree = BeautifulSoup(raw_html,  features = 'html.parser')
+tree = BeautifulSoup(raw_html, features='html.parser')
 img_link = (tree.find('img')).attrs['src']
 img_link = f'https://apod.nasa.gov/apod/{img_link}'
 img_data = requests.get(img_link).content
