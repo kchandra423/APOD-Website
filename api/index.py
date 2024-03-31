2330d1614066654a733323e2f100d80573d4a284
Kumar.s.Chandra <kumarschandra04@gmail.com>
2024-03-31 15:26:30 -0700

    stuff

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import flask
+from flask import Flask, redirect, Response
 
 import requests
 from bs4 import BeautifulSoup
@@ -6,13 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-
     raw_html = requests.get('https://apod.nasa.gov/apod/astropix.html').text
     tree = BeautifulSoup(raw_html, features='html.parser')
     img_link = (tree.find('img')).attrs['src']
     img_link = f'https://apod.nasa.gov/apod/{img_link}'
-
-    return f"<img src = '{img_link}'>" \
+    data = requests.get(img_link).content
+    # f = open('current.jpg', 'wb')
+    # f.write(data)
+    # f.close()
+    # f"<img src = '{img_link}', alt = 'amongus', style='vertical-align:middle'>" \
+    return  f"<img src = '{img_link}', alt = 'amongus', width = 100%, height = 100%, margin=0px>"
 
 if __name__ == '__main__':
     app.run()
